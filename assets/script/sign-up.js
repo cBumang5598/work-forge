b5a43a4e758e7affc9e9b910c8ef8ba4c014aff4
Carlos <cbumang@student.mitt.ca>
2025-12-12 03:37:01 -0600

    Completed javascript for signup

--- assets/script/sign-up.js
+++ assets/script/sign-up.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const newUserInput = document.querySelector("#newUser");
+const newPassInput = document.querySelector("#newPass");
+const signupForm = document.querySelector(".login-form");
+const errorMsg = document.querySelector("#error");
+
+signupForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const user = newUserInput.value.trim();
+  const pass = newPassInput.value.trim();
+
+  if (!user && !pass) {
+    errorMsg.textContent = "Please fill out all fields";
+    errorMsg.style.color = "red";
+    return;
+  }
+
+  localStorage.setItem("login_username", user);
+  localStorage.setItem("login_password", pass);
+
+  window.location.href = "index.html";
+});
